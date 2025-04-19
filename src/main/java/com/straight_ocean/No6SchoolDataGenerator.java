92515568ff0bd1cf9bcc44c23e417cfca42d2bd3
Straight_Ocean <xzy13399609049@163.com>
2025-04-19 20:57:15 +0800

    init

---
+++ src/main/java/com/straight_ocean/No6SchoolDataGenerator.java
@@ -0,0 +1,11 @@
+package com.straight_ocean;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class No6SchoolDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
